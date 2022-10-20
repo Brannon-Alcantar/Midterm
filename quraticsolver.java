1b181957ab1af139db36b775e88f5040fa5e89c1
Brannon-Alcantar <brannonalc@gmail.com>
2022-10-20 17:52:30 -0400

    Changed a,b,and c values again

--- quraticsolver.java
+++ quraticsolver.java
@@ -3,9 +3,9 @@ public class quraticsolver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 5;
-		int b = 8;
-		int c = 23;
+		int a = 6;
+		int b = 4;
+		int c = 50;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
